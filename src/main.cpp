1b5de0218d9802ff068cfb119270fe94fc70fc7c
tauzn-clock <daoxin.zhong@hmc.ox.ac.uk>
2023-09-18 21:28:01 +0100

    clean up

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <SPI.h>
-#include "../include/screen/screen.cpp"
+//#include "../include/screen/screen.cpp"
 #include "../include/imu/imu.cpp"
 #include "../include/wheel/wheel.cpp"
 #include "../include/control/balance.cpp"
@@ -10,7 +10,6 @@
 
 #define DEBUG false
 
-//SCREEN screen;
 IMU imu;
 WHEEL wheel1(D7,D0,D1);
 WHEEL wheel2(D8,D2,D3);
@@ -19,33 +18,28 @@ MANUAL manual;
 
 void setup() 
 {    
-
    Serial.begin(115200);
    Wire.begin(D6,D5);
-
-   //screen.initScreen();
-   //screen.writeText("Starting System",0,0);
    imu.initIMU();
    wheel1.initWheel();
    wheel2.initWheel();
 
-   setupWifi();
+   //setupWifi(); TODO: Wifi Control
 }
 
 void loop()
 { 
-   //delay(1000);
-   //screen.clearDisplay();
    imu.getEvents(); 
    float balanceMag = balance.balanceController(imu.a.acceleration,imu.g.gyro);
-   //balanceMag = 0;
+   wheel1.setSpeed(balanceMag);
+   wheel2.setSpeed(balanceMag);
+   /*
+   TODO: Wifi Control
    int direction = loopWifi();
-
    manual.handleInput(direction);
-   //Serial.printf("%f/n",balanceMag + manual.leftWheel);
    wheel1.setSpeed(balanceMag + manual.leftWheel);
    wheel2.setSpeed(balanceMag + manual.rightWheel);
-
+   */
 
    if (DEBUG){
       Serial.printf("Accel.x: %f\n",imu.a.acceleration.x);
@@ -54,23 +48,8 @@ void loop()
       float accel_mag = imu.a.acceleration.x*imu.a.acceleration.x + imu.a.acceleration.y*imu.a.acceleration.y + imu.a.acceleration.z*imu.a.acceleration.z;
       accel_mag = pow(accel_mag,0.5);
       Serial.printf("Accel Mag:%f\n",accel_mag);
-      //Serial.printf("Gyro.x: %f\n",imu.g.gyro.x);
       Serial.printf("Gyro.y: %f\n",imu.g.gyro.y);
-      //Serial.printf("Gyro.z: %f\n",imu.g.gyro.z);
-      //Serial.printf("Mag %f\n",mag);
       Serial.printf("Angle: %f\n",balance.getTheta(imu.a.acceleration));
       Serial.println("Still Alive");
-
-      /*
-      screen.writeText(imu.a.acceleration.x,0,0);
-      screen.writeText(imu.a.acceleration.y,40,0);
-      screen.writeText(imu.a.acceleration.z,80,0);
-
-      screen.writeText(imu.g.gyro.x,0,20);
-      screen.writeText(imu.g.gyro.y,40,20);
-      screen.writeText(imu.g.gyro.z,80,20);
-
-      screen.writeText(imu.temp.temperature,0,40);
-      */
    }
 }
